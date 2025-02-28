40ffd5ad0868bf9f8c8a1ca8764fdae9adcd7036
sk <you@example.com>
2025-02-28 15:43:17 +0530

    this is real git ka kaam

---
+++ src/models/subscription.models.js
@@ -0,0 +1,14 @@
+import mongoose,{Schema} from "mongoose"
+
+const SubscriptionSchema = new Schema({
+       subscriber:{
+        type:Schema.Types.ObjectId;
+        ref:"User"
+       },
+       channel:{
+        types:Schema.Types.ObjectId;
+        ref:"User"
+       }
+},{timestamps:true});
+
+export const Subscription = mongoose.model("Subscription",SubscriptionSchema);
